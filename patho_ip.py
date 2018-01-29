fd7dc3d1f0786390e8ad74058f1ab7b3a38cc2fc
Paul Holser <pholser@alumni.rice.edu>
2018-01-28 18:53:16 -0600

    WIP on patho IP program

--- patho_ip.py
+++ patho_ip.py
@@ -2,22 +2,46 @@ from __future__ import division
 from pyomo.environ import *
 from pyomo.opt import SolverFactory
 
+class Node:
+    def __init__(self, solution):
+        self.z = solution.z()
+        self.x = [solution.x[i]() for i in xrange(1, len(solution.x) + 1)]
+
+    def is_integer(self):
+        return all([v.is_integer() for v in self.x])
+
+    def branching_var(self):
+        return next(i for i, v in enumerate(self.x) if not v.is_integer()) + 1
+
+    def __str__(self):
+        return "z = %f, x = %s" % (self.z, self.x)
+
+    def __repr__(self):
+        return self.__str__()
+
+class BranchAndBound:
+    def __init__(self, problem):
+        self.problem = problem
+        self.opt = SolverFactory('cplex')
+
+    def solve(self):
+        z_star = -float('inf')
+        root_result = self.opt.solve(self.problem)
+        nodes = [Node(self.problem)]
+        return nodes
+
 def z(model):
     return model.x[1]
 
-def cx(model):
-    return 2*summation(model.x) == 5
+def model_instance():
+    model = AbstractModel()
+    model.n = 5
+    model.x = Var(RangeSet(model.n), within=NonNegativeReals, bounds=(0, 1))
+    model.z = Objective(rule=z)
+    model.constraints = ConstraintList()
+    instance = model.create_instance()
+    instance.constraints.add(2*summation(instance.x) == instance.n)
+    return instance
 
-opt = SolverFactory('cplex')
-
-model = ConcreteModel()
-model.n = 5
-model.indexes = RangeSet(1, model.n)
-model.x = Var(model.indexes, within=NonNegativeReals, bounds=(0, 1))
-model.z = Objective(rule=z)
-model.constraints = ConstraintList()
-model.constraints.add(2*summation(model.x) == 5)
-
-results = opt.solve(model)
-model.display()
-
+branch_and_bound = BranchAndBound(model_instance())
+print branch_and_bound.solve()
