928f951d3c834eb499a6c380ec1fffc3d1c3c398
Paul Holser <pholser@alumni.rice.edu>
2018-02-12 12:52:02 -0600

    Documenting MZN models for HW2

--- hw2-3a.py
+++ hw2-3a.py
@@ -28,27 +28,29 @@ class Job(namedtuple('Job', ['number', 'processing_time'])):
         return self.__str__()
 
 
-parameters = {}
-execfile("./hw2-3.dzn", {}, parameters)
+if __name__ == '__main__':
+    parameters = {}
+    execfile("./hw2-3.dzn", {}, parameters)
 
-number_of_machines = parameters['number_of_machines']
-number_of_jobs = parameters['number_of_jobs']
-processing_time = parameters['processing_time']
-assert number_of_jobs == len(processing_time)
+    number_of_machines = parameters['number_of_machines']
+    number_of_jobs = parameters['number_of_jobs']
+    processing_time = parameters['processing_time']
+    assert number_of_jobs == len(processing_time)
 
-machines = [Machine(i) for i in xrange(1, number_of_machines+1)]
-jobs = [Job(number=i+1, processing_time=v)
-    for i, v in enumerate(processing_time)
-]
-for j in sorted(jobs, key=lambda j: j.processing_time):
-    min(machines, key=lambda v: v.last_completion_time).add(j)
+    machines = [Machine(i) for i in xrange(1, number_of_machines+1)]
+    jobs = [Job(number=i+1, processing_time=v)
+        for i, v in enumerate(processing_time)
+    ]
+    for j in sorted(jobs, key=lambda j: j.processing_time):
+        min(machines, key=lambda v: v.last_completion_time).add(j)
 
-for m in machines:
-    print "\n\t".join([
-        'Machine %d:',
-        '%s',
-        'completion times: %s',
-        'last completion time: %d'
-    ]) % (m.number, m.jobs, m.completion_times(), m.last_completion_time)
-print "Sum of all completion times:", \
-    sum([sum(m.completion_times()) for m in machines])
+    for m in machines:
+        print "\n\t".join([
+            'Machine %d:',
+            '%s',
+            'completion times: %s',
+            'last completion time: %d'
+        ]) % (m.number, m.jobs, m.completion_times(), m.last_completion_time)
+    print "Sum of all completion times:", \
+        sum([sum(m.completion_times()) for m in machines])
+
