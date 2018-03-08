5a22b7dec120aaf512faf8308776dcdc3e0c93c3
Paul Holser <pholser@alumni.rice.edu>
2018-03-08 09:23:39 -0600

    HW4 wip

--- hw4/hw4-2.py
+++ hw4/hw4-2.py
@@ -3,20 +3,24 @@ import numpy as np
 import sys
 
 cover = tuple(sorted(map(lambda i: int(i) - 1, sys.argv[1:])))
+extended_cover = tuple(range(0, cover[0])) + cover
 
-x = [35, 27, 23, 19, 15, 15, 12, 8, 6, 3]
-alpha = [2, 2, 1, 1, 1, 1, 1, 1, 1, 0]
+print "extended cover:", extended_cover
+
+x = [36, 27, 12, 6, 4, 2]
 
 def feasible(point):
-    return sum(x[i] for i in point) <= 39
+    return sum(x[i] for i in point) == 39
 
-def meets_cover(point):
-    return sum(alpha[i] * int(bool(i in cover)) for i in point) == 3
+def meets_at_equality(point):
+    return True
+    # return sum(int(bool(i in extended_cover)) for i in point) \
+    #     == len(cover) - 1
 
 vectors = []
 for i in xrange(1, len(x)+1):
     for j in it.combinations(xrange(0, len(x)), i):
-        if feasible(j) and meets_cover(j):
+        if feasible(j) and meets_extended_cover(j):
             vectors.append([int(bool(k in j)) for k in xrange(0, len(x))] + [1])
 
 vectors.sort()
