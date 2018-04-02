5ce08a3e536c75e3f67cf6d660b9b41517c97b39
Paul Holser <pholser@alumni.rice.edu>
2018-04-01 20:47:58 -0500

    Add data sets

---
+++ project/py/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vertex-coloring',
+    version='1.0.0',
+    description='IP solver for vertex coloring problem in graphs',
+    author='Paul Holser',
+    author_email='pholser@ksu.edu',
+    packages=find_packages(),
+    install_requires=[
+        'networkx>=2.1',
+        'pytest>=3.5.0'
+    ]
+)
