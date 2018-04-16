030e1ce2ad4f9f0e5661f534e486a12ec5496aa3
Paul Holser <pholser@alumni.rice.edu>
2018-04-16 10:49:00 -0500

    Project: solution test for rep formulation

---
+++ project/py/tests/test_representative_solutions.py
@@ -0,0 +1,38 @@
+import pytest
+
+from vertexcoloring.dimacs.parser import Parser
+from vertexcoloring.formulation.representative.problem import Problem
+
+
+@pytest.fixture
+def small_graph():
+    return Parser().parse('tests/data/gc_4_1.col')
+
+
+@pytest.fixture
+def small_problem(small_graph):
+    problem = Problem(small_graph, 'lr')
+    problem.suppress_output()
+    return problem
+
+
+@pytest.fixture
+def small_solution(small_problem):
+    return small_problem.solve()
+
+
+def test_objective(small_solution):
+    assert 2.0 == small_solution.objective_value()
+
+
+def test_color_markings(small_solution):
+    used_colors = small_solution.used_colors()
+    assert small_solution.objective_value() == len(used_colors)
+
+    node_colors = small_solution.colors_by_node()
+    assert set(used_colors) == set(node_colors.values())
+
+    by_color = small_solution.nodes_by_color()
+    color_classes = by_color.values()
+    assert [0, 2, 3] in color_classes
+    assert [1, 4] in color_classes
