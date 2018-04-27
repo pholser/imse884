cd9a7eeb828993e6d80cafcba2aa5fd829fea482
Paul Holser <pholser@alumni.rice.edu>
2018-04-27 13:20:20 -0500

    Remove cplex from setup.py. Too much hassle.

--- project/py/setup.py
+++ project/py/setup.py
@@ -8,7 +8,6 @@ setup(
     author_email='pholser@ksu.edu',
     packages=find_packages(),
     install_requires=[
-        'cplex>=12.8.0.0',
         'networkx>=2.1',
         'matplotlib>=2.2.2',
         'pytest>=3.5.0'
